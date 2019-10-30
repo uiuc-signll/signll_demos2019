741643aee4f367ef75ad7a819fe380b1a2821d90
jasonxia17 <jasonx3@illinois.edu>
2019-10-30 12:18:53 -0500

    XOR: Implemented 2-layer neural net

--- xor_neural_net/model.py
+++ xor_neural_net/model.py
@@ -13,11 +13,37 @@ class NeuralNet(torch.nn.Module):
     def __init__(self):
         super(NeuralNet, self).__init__()
         
-        self.weight_vec = torch.nn.Parameter(torch.randn(2))
-        self.bias = torch.nn.Parameter(torch.randn(1))
+        self.layer1weight0 = torch.nn.Parameter(torch.randn(2))
+        self.layer1weight1 = torch.nn.Parameter(torch.randn(2))
+        self.layer2weight = torch.nn.Parameter(torch.randn(2))
+
+        self.layer1bias0 = torch.nn.Parameter(torch.randn(1))
+        self.layer1bias1 = torch.nn.Parameter(torch.randn(1))
+        self.layer2bias = torch.nn.Parameter(torch.randn(1))
 
     def forward(self, x):
-        return self.weight_vec @ x + self.bias
+        h = torch.zeros(2)
+
+        h[0] = x @ self.layer1weight0 + self.layer1bias0
+        h[1] = x @ self.layer1weight1 + self.layer1bias1
+
+        if h[0] < 0:
+            h[0] *= 0.01
+        if h[1] < 0:
+            h[1] *= 0.01
+        
+        return h @ self.layer2weight + self.layer2bias
+
+class NeuralNetUsingBuiltInModules(torch.nn.Module):
+    def __init__(self):
+        super(NeuralNetUsingBuiltInModules, self).__init__()
+
+        self.l1 = torch.nn.Linear(2, 2)     # can easily increase number of nodes in hidden layer
+        self.l2 = torch.nn.Linear(2, 1)
+
+    def forward(self, x):
+        from torch.nn.functional import relu
+        return self.l2(relu(self.l1(x)))
 
 classifier = NeuralNet()
 
@@ -41,6 +67,9 @@ for epoch in range(1000):
         loss.backward()         # calculate the gradients
         optimizer.step()        # take step down the gradient
         optimizer.zero_grad()   # reset the gradients to zero
+
+        if epoch % 143 == 0:
+            print(loss)
 
 for i in range(len(X)):
     prediction = classifier.forward(X[i])
